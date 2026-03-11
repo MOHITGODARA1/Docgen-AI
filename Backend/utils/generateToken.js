89b80d51e7f50fb8c5135f19e1018cc4315bf3d7
Mohit Godara <mohitgodara@Mohits-MacBook-Air-3.local>
2026-03-11 13:37:57 +0530

    work on deploy bug

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 };
